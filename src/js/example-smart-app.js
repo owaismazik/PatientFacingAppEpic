e30ab78eb5740f645b68fbc521eba7f2589223b3
owaismazik <63541338+owaismazik@users.noreply.github.com>
2020-11-16 16:18:44 +0500

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -15,7 +15,7 @@
                 var pt = patient.read();
 
                 var obv = smart.patient.api.fetchAll({
-                    type: 'AllergyIntolerance',
+                    type: 'Observation',
                     query: {
                         code: {
                             $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
